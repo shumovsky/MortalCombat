5662925498ac64fda3df5f89227b83bef77ded3e
Dmitry Shumovsky <d.shumovsky@bk.ru>
2021-06-04 23:02:52 +0300

    finish homework-2

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 const $player1 = {
-    name: 'SUB-ZERO',
+    name: 'Liukang',
     hp: 100,
-    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: ['head'],
-    attack: () => {
-        alert(name + 'Fight...');
+    attack: function() {
+        alert(this.name + ' Fight...');
     },
 };
+
+
 const $player2 = {
     name: 'SCORPION',
     hp: 80,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['foot'],
-    attack: () => {
-        alert(name + 'Fight...');
+    attack: function() {
+        alert(this.name + ' Fight...');
     },
 };
 
-function createPlayer() {
+function createPlayer(player, hero) {
+    const $arenas = document.querySelector('.arenas');
+
     const $divPlayer1 = document.createElement('div');
-    $divPlayer1.classList.add('player1');
+    $divPlayer1.classList.add(player);
 
     const $progressBar = document.createElement('div');
-    $progressBar.classList.add('progressBar');
+    $progressBar.classList.add('progressbar');
 
     const $character = document.createElement('div');
     $character.classList.add('character');
@@ -33,9 +37,10 @@ function createPlayer() {
     const $name = document.createElement('div');
     $name.classList.add('name');
 
-    const $img = document.createElement('div');
+    const $img = document.createElement('img');
     $img.classList.add('img');
 
+    $arenas.appendChild($divPlayer1);
 
     $divPlayer1.appendChild($progressBar);
     $divPlayer1.appendChild($character);
@@ -44,4 +49,12 @@ function createPlayer() {
     $progressBar.appendChild($name);
 
     $character.appendChild($img);
+
+    $life.style.width = hero.hp + '%';
+    $name.innerText = hero['name'];
+    $img.src = hero.img;
 }
+
+
+createPlayer('player1', $player1);
+createPlayer('player2', $player2);
